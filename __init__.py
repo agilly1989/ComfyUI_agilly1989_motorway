32d58397d5835e27fbc7414a15147e7c70ecc577
agilly1989 <agilly1989@gmail.com>
2025-08-03 21:26:02 +1000

    HOTFIX - Monkeypatch removed (UNTESTED)

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 
 from .ramps.generated import NODE_CLASS_MAPPINGS as GeneratedMapping
 from .util_nodes.primitives import NODE_CLASS_MAPPINGS as PrimitiveMapping
-from .clone_nodes_in_the_dangerzone.nodes import NODE_CLASS_MAPPING as ClonedNodeMapping
-
-NODE_CLASS_MAPPINGS = ClonedNodeMapping
+#from .clone_nodes_in_the_dangerzone.nodes import NODE_CLASS_MAPPING as ClonedNodeMapping
+NODE_CLASS_MAPPINGS = {}
+#NODE_CLASS_MAPPINGS.update(ClonedNodeMapping)
 NODE_CLASS_MAPPINGS.update(GeneratedMapping)
 NODE_CLASS_MAPPINGS.update(PrimitiveMapping)
